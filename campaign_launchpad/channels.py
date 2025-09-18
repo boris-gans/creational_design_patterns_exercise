a35e3d71f71d9be087250b604549ef8360e406c7
studentBorisGans <bgans.ieu2023@student.ie.edu>
2025-09-18 10:42:40 +0200

    all channels tests passed

--- campaign_launchpad/channels.py
+++ campaign_launchpad/channels.py
@@ -4,31 +4,57 @@ from uuid import uuid4
 from .budget import GlobalBudget
 from .campaign import Campaign
 
+# Functional requirements:
+# - Campaigns should allocate their daily budget to the global budget.
+# - When a campaign is created, a external id should be returned. This id should have a prefix with the initial of the channel i.e. For facebook ads --> "f_<some_id>"
+# - Two clients are needed: FacebookAds and GoogleAds
+# - If different channel client is created, and error should arise.
 
 class ChannelClient(ABC):
+    # _id_counter = uuid4()
+
     def __init__(self, name: str):
-        self.name = name
+      self.name = name
+      self.budget = GlobalBudget()
 
     @abstractmethod
     def create_campaign(self, campaign: Campaign) -> str:
-        # TODO: Create a campaign on this channel and return an external id.
-        pass
+      pass
 
     @abstractmethod
     def pause_campaign(self, campaign_id: str) -> None:
-        pass
+      pass
 
 
 class GoogleAdsClient(ChannelClient):
-  # TODO: Implement the Google Ads specific logic here.
-  pass
+  def __init__(self):
+     super().__init__("google")
+  
+  def create_campaign(self, campaign):
+    self.budget.allocate(campaign.daily_budget)
+    return f"g-{uuid4().hex}"
+
+  def pause_campaign(self, campaign_id):
+    print(f"Paused Google campaign with id {campaign_id}")
+
 
 class FacebookAdsClient(ChannelClient):
-  # TODO: Implement the Facebook Ads specific logic here.
-  pass
+  def __init__(self):
+     super().__init__("facebook")
+
+  def create_campaign(self, campaign):
+    self.budget.allocate(campaign.daily_budget)
+    return f"f-{uuid4().hex}"
+
+  def pause_campaign(self, campaign_id):
+    print(f"Paused Facebook campaign with id {campaign_id}")
 
 class ChannelClientFactory:
     @staticmethod
     def create(channel: str) -> ChannelClient:
-      # TODO: Return the appropriate client based on the channel.
-      pass
+      if channel.lower() == "google":
+         return GoogleAdsClient()
+      elif channel.lower() == "facebook":
+         return FacebookAdsClient()
+      else:
+         raise ValueError("Improper channel entered")
