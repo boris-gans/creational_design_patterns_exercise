1d83668618e794d9ca96868e51bd2aa9ca1727bd
studentBorisGans <bgans.ieu2023@student.ie.edu>
2025-09-18 10:23:28 +0200

    budget done 4/4

--- campaign_launchpad/budget.py
+++ campaign_launchpad/budget.py
@@ -3,25 +3,23 @@ class GlobalBudget:
     """
     One shared marketing budget across the system.
     """
-# Functional requirements:
-# - Ensure there is only a single bugdet
-# - Ensure bugdet cannot go below zero
-# - Allow no negative allocations
+
     _instance = None
 
     def __new__(cls, initial_amount: float = 0.0):
-      # TODO: Singleton pattern implementation
 
       if cls._instance is None and initial_amount > 0:
          cls._instance = super().__new__(cls)
          cls._instance._balance = initial_amount
       return cls._instance
-      # pass
+
 
     def allocate(self, amount: float) -> None:
-      # TODO: Allocate amount from the budget
-      if amount > 0:
+      if amount > 0 and self._balance - amount > 0:
         self._balance -= amount
+      else:
+        raise ValueError("Cannot allocate more than budget")
+  
       return self._balance
 
     def remaining(self) -> float:
